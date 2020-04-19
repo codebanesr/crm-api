9566373e7a4fb638bd72d1f3143016dbdae6a331
shanur rahman <shanur.rahman@gamechangesns.com>
2020-04-19 23:10:25 +0400

    resolving build issues on github

--- src/app.ts
+++ src/app.ts
@@ -5,25 +5,18 @@ import passport from "passport";
 import * as homeController from "@controllers/home";
 import * as apiController from "@controllers/api";
 
-// API keys and Passport configuration
 import * as passportConfig from "@config/passport";
+import initRoutes from "@starter/initRoutes";
+import dbInit from "@starter/dbInit";
+import pluginStarter from "@starter/plugins"
 
-// Create Express server
 const app = express();
 
-// middleware initialization step
-require('@starter/plugins')(app);
+pluginStarter(app);
+dbInit(app);
+initRoutes(app);
 
-// database initialization
-require('@starter/dbInit')(app);
 
-/**
- * Primary app routes.
- */
-require('@starter/initRoutes')(app);
-/**
- * API examples routes.
- */
 app.get("/", homeController.index);
 app.get("/api", apiController.getApi);
 app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
