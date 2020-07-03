1648028bea23b675d11938324523b56519ac053e
shanurrahman <shanur.rahman@gamechangesns.com>
2020-07-04 01:06:01 +0400

    setting up production environment

--- src/controllers/role.ts
+++ src/controllers/role.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express"; 
+import { Request, Response } from "express"; 
 import Role from "../models/Role";
 import Permission from "../models/permission";
 
