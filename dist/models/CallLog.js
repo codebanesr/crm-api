810c40df46ec04aecbe0611281d8fe684025dbfa
shanurrahman <shanur.rahman@gamechangesns.com>
2020-07-18 14:39:50 +0400

    fix: [SK-0000] minor fixed

--- dist/models/CallLog.js
+++ dist/models/CallLog.js
@@ -6,8 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const CallLog = new mongoose_1.default.Schema({
     _id: mongoose_1.default.Schema.Types.ObjectId,
+    "date": Date,
+    "number": String,
+    "type": Number,
+    "duration": Number,
+    "new": Number,
+    "cachedNumberType": Number,
+    "phoneAccountId": String,
+    "viaNumber": String,
+    "name": String,
+    "contact": String,
+    "photo": String,
+    "thumbPhoto": String,
 }, {
     strict: false
 });
+CallLog.index({ date: 1, number: 1 }, { unique: true });
 exports.default = mongoose_1.default.model("CallLog", CallLog);
 //# sourceMappingURL=CallLog.js.map
--- src/models/CallLog.ts
+++ src/models/CallLog.ts
@@ -2,8 +2,23 @@ import mongoose from "mongoose";
 
 const CallLog = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    "date" : Date,
+    "number" : String,
+    "type" : Number,
+    "duration" : Number,
+    "new" : Number,
+    "cachedNumberType" : Number,
+    "phoneAccountId" : String,
+    "viaNumber" : String,
+    "name" : String,
+    "contact" : String,
+    "photo" : String,
+    "thumbPhoto" : String,
 }, {
     strict: false
 });
+
+
+CallLog.index({date:1, number:1}, { unique: true });
 
 export default mongoose.model("CallLog", CallLog);
