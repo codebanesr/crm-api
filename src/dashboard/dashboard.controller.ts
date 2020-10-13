97e219d843b7b7b1485baf05b5aad4abc2ac5a64
shanur rahman <shanur.cse.nitap@gmail.com>
2020-10-14 00:53:41 +0530

    fix: [BN-0] query for getting leads on a date, week and month

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,29 +1,83 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Query, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
-import { CurrentUser } from '../auth/decorators/current-user.decorator';
-import { Roles } from '../auth/decorators/roles.decorator';
-import { CreateUserDto } from '../user/dto/create-user.dto';
-import { User } from '../user/interfaces/user.interface';
-import { DashboardService } from './dashboard.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Query,
+  UseGuards,
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { ApiOperation, ApiCreatedResponse } from "@nestjs/swagger";
+import { CurrentUser } from "../auth/decorators/current-user.decorator";
+import { Roles } from "../auth/decorators/roles.decorator";
+import { User } from "../user/interfaces/user.interface";
+import { DashboardService } from "./dashboard.service";
 
-@Controller('dashboard')
+@Controller("dashboard")
 export class DashboardController {
+  constructor(private readonly dashboardService: DashboardService) {}
 
-    constructor(private readonly dashboardService: DashboardService) {}
+  @Post("leadStatus")
+  @UseGuards(AuthGuard("jwt"))
+  @Roles("admin")
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    summary: `Get aggregated Lead Status for a given date range,  kitne amount ka transaction fail hua, 
+                    pass hua ya nurturing state me hai pie chart data `,
+  })
+  @ApiCreatedResponse({})
+  async getLeadStatusByOrganization(
+    @CurrentUser() user: User,
+    @Body() body: any
+  ) {
+    const {organization} = user;
+    const { dateArray } = body;
+    return this.dashboardService.getAggregatedLeadStatus(organization, dateArray);
+  }
 
+  @Post("leadStatus/department")
+  @UseGuards(AuthGuard("jwt"))
+  @Roles("admin")
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    summary:
+      `Get aggregated Lead Status for the department this manager belongs top, 
+                            This also accepts date filter in the form of array elements
+                            Top 3 frontline with highest number of leads and leads in respective stages
+      `,
+  })
+  @ApiCreatedResponse({})
+  async getLeadStatusByDepartment(
+    @CurrentUser() user: User,
+    @Body() body: any
+  ) {
+    const {organization} = user;
+    const { dateArray } = body;
+    return this.dashboardService.getAggrgegatedLeadStatusForDepartment(
+      organization,
+      dateArray
+    );
+  }
 
-    @Post('leadStatus')
-    @UseGuards(AuthGuard("jwt"))
-    @Roles("admin")
-    @HttpCode(HttpStatus.CREATED)
-    @ApiOperation({ summary: "Get aggregated Lead Status for a given date range" })
-    @ApiCreatedResponse({})
-    async register(
-      @CurrentUser() user: User,
-      @Body() body: any
-    ) {
-        const {dateArray} = body;
-        return this.dashboardService.getAggregatedLeadStatus(dateArray);
-    }
+  @Post("leadStatus/monthlyReport")
+  @UseGuards(AuthGuard("jwt"))
+  @Roles("admin")
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    summary: "Sends details about leads by week, month or cycle; new/lost/nurturing, input is the month, week and day calculation will be automatically done",
+  })
+  @ApiCreatedResponse({})
+  async getLeadInfoByMonth(
+    @CurrentUser() user: User,
+    @Body() body: any
+  ) {
+    const {organization} = user;
+    const { month } = body;
+    return this.dashboardService.getLeadInfoByMonth(
+      organization,
+      month
+    );
+  }
 }
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,56 +1,177 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Campaign } from '../campaign/interfaces/campaign.interface';
-import { CallLog } from '../lead/interfaces/call-log.interface';
-import { CampaignConfig } from '../lead/interfaces/campaign-config.interface';
-import { EmailTemplate } from '../lead/interfaces/email-template.interface';
-import { Lead } from '../lead/interfaces/lead.interface';
-import { User } from '../user/interfaces/user.interface';
-import { Model } from 'mongoose';
+import { Injectable, Logger } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Campaign } from "../campaign/interfaces/campaign.interface";
+import { CallLog } from "../lead/interfaces/call-log.interface";
+import { CampaignConfig } from "../lead/interfaces/campaign-config.interface";
+import { EmailTemplate } from "../lead/interfaces/email-template.interface";
+import { Lead } from "../lead/interfaces/lead.interface";
+import { User } from "../user/interfaces/user.interface";
+import { Model } from "mongoose";
 
 @Injectable()
 export class DashboardService {
-    constructor(
-        @InjectModel("Lead")
-        private readonly leadModel: Model<Lead>,
-    
-        @InjectModel("User")
-        private readonly userModel: Model<User>,
-    
-        @InjectModel("CampaignConfig")
-        private readonly campaignConfigModel: Model<CampaignConfig>,
-    
-        @InjectModel("Campaign")
-        private readonly campaignModel: Model<Campaign>,
-    
-        @InjectModel("EmailTemplate")
-        private readonly emailTemplateModel: Model<EmailTemplate>,
-    
-        @InjectModel("CallLog")
-        private readonly callLogModel: Model<CallLog>,    
-    ) {}    
+  constructor(
+    @InjectModel("Lead")
+    private readonly leadModel: Model<Lead>,
 
+    @InjectModel("User")
+    private readonly userModel: Model<User>,
 
+    @InjectModel("CampaignConfig")
+    private readonly campaignConfigModel: Model<CampaignConfig>,
 
+    @InjectModel("Campaign")
+    private readonly campaignModel: Model<Campaign>,
 
-    getAggregatedLeadStatus(dateArray) {
-        const leadAgg = this.leadModel.aggregate();
-        let startDate, endDate;
-        if (dateArray) {
-          [startDate, endDate] = dateArray;
-          startDate = new Date(startDate);
-          endDate = new Date(endDate);
-          leadAgg.match({
-            updatedAt: { $gte: startDate, $lt: endDate },
-          });
-        }
+    @InjectModel("EmailTemplate")
+    private readonly emailTemplateModel: Model<EmailTemplate>,
 
-        leadAgg.group( {
-            _id: {leadStatus: "$leadStatus"}, 
-            totalAmount:  {$sum: "$amount"}
-        });
+    @InjectModel("CallLog")
+    private readonly callLogModel: Model<CallLog>
+  ) {}
 
-        Logger.debug(leadAgg)
-        return leadAgg.exec()
+  getAggregatedLeadStatus(organization: string, dateArray: string[]) {
+    const leadAgg = this.leadModel.aggregate();
+    let startDate, endDate;
+    if (dateArray) {
+      [startDate, endDate] = dateArray;
+      startDate = new Date(startDate);
+      endDate = new Date(endDate);
+      leadAgg.match({
+        updatedAt: { $gte: startDate, $lt: endDate },
+      });
     }
+
+    leadAgg.match({ organization });
+    leadAgg.group({
+      _id: { leadStatus: "$leadStatus" },
+      totalAmount: { $sum: "$amount" },
+    });
+
+    return leadAgg.exec();
+  }
+
+  // top 3 performers
+  getAggrgegatedLeadStatusForDepartment(
+    organization: string,
+    dateArray: string[]
+  ) {
+    let startDate, endDate;
+    const aggregateQuery = this.leadModel.aggregate();
+    if (dateArray) {
+      [startDate, endDate] = dateArray;
+      startDate = new Date(startDate);
+      endDate = new Date(endDate);
+      aggregateQuery.match({
+        updatedAt: { $gte: startDate, $lt: endDate },
+      });
+    }
+
+    // only considering leads for his organization
+    aggregateQuery.match({ organization });
+
+    // grouping leads by user first and calculating count of leads for each user, and taking all fields that we need in the future
+    aggregateQuery.group({
+      _id: { email: "$email", leadStatus: "$leadStatus" },
+      count: { $sum: 1 },
+      amount: { $sum: "$amount" },
+    });
+
+    // now sort these users by number of leads they have
+    aggregateQuery.sort({ count: -1 });
+
+    // take the first 3 users
+    aggregateQuery.limit(3);
+
+    // format the data, take everything out of _id object that comes from aggregation
+    aggregateQuery.project({
+      email: "$_id.email",
+      leadStatus: "$_id.leadStatus",
+      count: "$count",
+      amount: "$amount",
+      _id: 0,
+    });
+
+    // now group by user to send this object to the frontend for consumption
+    aggregateQuery.group({
+      _id: "$email",
+      leadsWithStatus: { $push: "$$ROOT" },
+      allLeadCount: { $sum: "$amount" },
+    });
+
+    return aggregateQuery.exec();
+  }
+
+  async getLeadInfoByMonth(organization: string, month: number) {
+    // apply filter by leadStatus and so on here for the user
+    const leadAgg = this.leadModel.aggregate();
+
+    // filter leads for the organization under review
+    leadAgg.match({ organization: organization });
+
+    // remove all timestamps and
+    // project all usable fields
+    leadAgg.project({
+      year: {
+        $year: "$createdAt",
+      },
+      month: {
+        $month: "$createdAt",
+      },
+      week: {
+        $week: "$createdAt",
+      },
+      day: {
+        $dayOfWeek: "$createdAt",
+      },
+      weight: { $literal: 1 },
+      amount: "$amount",
+    });
+
+
+    // day based aggregation and grouping by day
+    leadAgg.group({
+      _id: {
+        year: "$year",
+        month: "$month",
+        week: "$week",
+        day: "$day",
+      },
+      totalWeightDaily: {
+        $sum: "$weight",
+      },
+      totalAmountDaily: {
+        $sum: "$amount",
+      },
+    });
+
+    // week based aggregation and grouping by day
+    leadAgg.group({
+      _id: {
+        year: "$_id.year",
+        month: "$_id.month",
+        week: "$_id.week",
+      },
+      totalWeightWeekly: {
+        $sum: "$totalWeightDaily",
+      },
+      totalAmountWeekly: {
+        $sum: "$totalAmountDaily",
+      },
+      totalWeightDay: {
+        $push: {
+          totalWeightDay: "$totalWeightDaily",
+          totalAmountDay: "$totalAmountDaily",
+          dayOfWeek: "$_id.day",
+        },
+      }
+    });
+
+    // return results for month under query
+    leadAgg.match({
+      "_id.month": month,
+    });
+
+    return leadAgg.exec();
+  }
 }
