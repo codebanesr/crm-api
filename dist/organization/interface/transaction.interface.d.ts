37e91262320304375d97e6e53ee45370fcde9632
shanur rahman <shanur.cse.nitap@gmail.com>
2021-03-28 20:34:09 +0530

    using system cache to build images, copying yarn.lock is very important

--- dist/organization/interface/transaction.interface.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from "mongoose";
-export interface Transaction extends Document {
-    organization: string;
-    discount: number;
-    expiresOn: Date;
-    perUserRate: number;
-    seats: number;
-    total: number;
-}
--- dist/user/dto/create-user.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { RoleType } from "../../shared/role-type.enum";
-export declare class CreateUserDto {
-    readonly fullName: string;
-    readonly email: string;
-    password: string;
-    readonly roleType: RoleType;
-    readonly reportsTo?: string;
-    phoneNumber: string;
-}
